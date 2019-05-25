675367a4acd1bf8272abb05c85c6589bb744eaba
EliTu <eliad91@gmail.com>
2019-05-25 19:30:12 +0800

    Starting exercise 1

--- Section 3/Assignment 1/base-syntax--assignment-problem/src/App.js
+++ Section 3/Assignment 1/base-syntax--assignment-problem/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ol>
-          <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
+	render() {
+		return (
+			<div className="App">
+				<h1>Assignment 1</h1>
+				<UserOutput />
+				<UserOutput />
+				<UserOutput />
+
+				{/* <ol>
           <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
           <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
           <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
@@ -16,10 +20,10 @@ class App extends Component {
           <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
           <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
           <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
-        </ol>
-      </div>
-    );
-  }
+        </ol> */}
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ Section 3/Assignment 1/base-syntax--assignment-problem/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserOutput = () => {
+	return (
+		<div>
+			<p>This is the first user output</p>
+			<p>This is the second user output</p>
+		</div>
+	);
+};
+
+export default UserOutput;

63b1f97b872c2764683d2d1c29b5daead5baf96a
EliTu <eliad91@gmail.com>
2019-06-06 19:23:59 +0800

    Finished lecture 151, added the basic file structure and the Layout component

---
+++ Section 8/burger-builder-app/src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Layout = props => {
+	return (
+		<>
+			<div>
+				{/* Toolbar */}
+				{/* SideDrawer */}
+				{/* BackDrop */}
+			</div>
+			<main>{props.children}</main>
+		</>
+	);
+};
+
+export default Layout;

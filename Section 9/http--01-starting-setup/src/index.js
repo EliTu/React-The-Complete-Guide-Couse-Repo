0af876168079eed8ac4be9df795350670f142759
EliTu <eliad91@gmail.com>
2019-06-16 15:39:37 +0800

    Finished lecture 199

--- Section 9/http--01-starting-setup/src/index.js
+++ Section 9/http--01-starting-setup/src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+// Setting axios interceptor:
+const configInterceptor = axios.interceptors.request.use(
+	config => {
+		console.log(config);
+		return config;
+	},
+	error => {
+		console.log(error);
+		return Promise.reject(error);
+	}
+);
+
+const responseInterceptor = axios.interceptors.response.use(
+	config => {
+		console.log(config);
+		return config;
+	},
+	error => {
+		console.log(error);
+		return Promise.reject(error);
+	}
+);
+
+// Removing axios interceptors:
+axios.interceptors.request.eject(configInterceptor);
+axios.interceptors.response.eject(responseInterceptor);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

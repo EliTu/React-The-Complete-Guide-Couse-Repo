5008fc6d9ec3a32252624c1b882d3f866c5f974f
EliTu <eliad91@gmail.com>
2019-06-19 16:10:53 +0800

    Finished lecture 222 and Added About component

---
+++ Section 11/http--01-starting-setup/src/components/About/About.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+	return (
+		<div className="about">
+			<h1>About me:</h1>
+			<div>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+					Eum eaque odio dicta quo ullam, inventore neque ex. Nihil
+					adipisci labore, consequuntur in quibusdam dolores, aliquid
+					quas maxime laudantium ipsum nulla!
+				</p>
+			</div>
+			<div>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+					Reprehenderit, odio!
+				</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+					Atque repudiandae assumenda iure. Pariatur, eligendi
+					cupiditate voluptas ullam illo labore. Voluptates aliquid ex
+					in ipsam, non labore nobis illo et laboriosam expedita
+					dolorem nemo facilis vitae consequatur ipsa fugiat repellat
+					quod!
+				</p>
+			</div>
+			<div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Officia nihil hic sed ut nostrum aliquid, voluptates
+					quibusdam. Exercitationem veniam similique tempora vitae
+					harum? Eum, commodi.
+				</p>
+			</div>
+			<div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Voluptate corrupti sit, sed enim soluta a.
+				</p>
+			</div>
+		</div>
+	);
+}
+
+export default About;

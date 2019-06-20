15a323b52d7b890afea91fa4966ea461495766b8
EliTu <eliad91@gmail.com>
2019-06-20 19:26:52 +0800

    In the middle of the assignment

--- Section 11/routing--assignment-problem/src/App.js
+++ Section 11/routing--assignment-problem/src/App.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-
+import { Route, NavLink, Switch } from 'react-router-dom';
 import Courses from './containers/Courses/Courses';
+import Course from './containers/Course/Course';
 import Users from './containers/Users/Users';
 
 class App extends Component {
-  render () {
-    return (
-      <div className="App">
-        <ol style={{textAlign: 'left'}}>
-          <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
-          <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
-          <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
-          <li>Pass the course ID to the "Course" page and output it there</li>
-          <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
-          <li>Load the "Course" component as a nested component of "Courses"</li>
-          <li>Add a 404 error page and render it for any unknown routes</li>
-          <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
-        </ol>
-      </div>
-    );
-  }
+	render() {
+		console.log(this.props);
+		return (
+			<div className="App">
+				<div>
+					<ul className="Nav">
+						<li>
+							<NavLink to="/">Home</NavLink>
+						</li>
+						<li>
+							<NavLink to="/courses">Courses</NavLink>
+						</li>
+						<li>
+							<NavLink to="/users">Users</NavLink>
+						</li>
+					</ul>
+				</div>
+				<div>
+					<Switch>
+						<Route path="/courses" exact component={Courses} />
+						<Route path="/users" exact component={Users} />
+						<Route path="/courses/:id" exact component={Course} />
+					</Switch>
+				</div>
+				<hr />
+				<ol style={{ textAlign: 'left' }}>
+					{/* <li>
+							Add Routes to load "Users" and "Courses" on
+							different pages (by entering a URL, without Links)
+						</li> */}
+					{/* <li>
+							Add a simple navigation with two links => One
+							leading to "Users", one leading to "Courses"
+						</li> */}
+					{/* <li>
+						Make the courses in "Courses" clickable by adding a link
+						and load the "Course" component in the place of
+						"Courses" (without passing any data for now)
+					</li> */}
+					{/* <li>
+						Pass the course ID to the "Course" page and output it
+						there
+					</li> */}
+					<li>
+						Pass the course title to the "Course" page - pass it as
+						a param or score bonus points by passing it as query
+						params (you need to manually parse them though!)
+					</li>
+					<li>
+						Load the "Course" component as a nested component of
+						"Courses"
+					</li>
+					<li>
+						Add a 404 error page and render it for any unknown
+						routes
+					</li>
+					<li>
+						Redirect requests to /all-courses to /courses (=> Your
+						"Courses" page)
+					</li>
+				</ol>
+			</div>
+		);
+	}
 }
 
 export default App;
--- Section 11/routing--assignment-problem/src/containers/Course/Course.js
+++ Section 11/routing--assignment-problem/src/containers/Course/Course.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import queryString from 'query-string';
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
-    }
+	componentDidMount() {
+		const title = queryString.parse(this.props.location.search, {
+			ignoreQueryPrefix: true,
+		});
+		console.log(title);
+	}
+
+	render() {
+		const { match, location } = this.props;
+		console.log(location);
+
+		return (
+			<div>
+				<h1>_COURSE_TITLE_</h1>
+				<p>You selected the Course with ID: {match.params.id} </p>
+			</div>
+		);
+	}
 }
 
-export default Course;
+export default withRouter(Course);

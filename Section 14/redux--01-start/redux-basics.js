0e2f5423176d9180ac2cb7c30ce671faade1a836
EliTu <eliad91@gmail.com>
2019-06-28 16:37:11 +0800

    Finished lecture 283

--- Section 14/redux--01-start/redux-basics.js
+++ Section 14/redux--01-start/redux-basics.js
@@ -1,18 +1,42 @@
+// For using Node.js:
 const redux = require('redux');
+
 const createStore = redux.createStore;
 
 const initialState = {
 	counter: 0,
 };
 
-// Reducer
+// 1: Reducer
 const rootReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'INC_COUNTER':
+			state = {
+				...state,
+				counter: state.counter + 1,
+			};
+			break;
+
+		case 'ADD_COUNTER':
+			state = {
+				...state,
+				counter: state.counter + action.value,
+			};
+			break;
+
+		default:
+			state;
+	}
 	return state;
 };
 
-// Store
+// 2: Store
 const store = createStore(rootReducer);
 console.log(store.getState());
-// Action
 
-// Subscription
+// 3: Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
+
+// 4: Subscription

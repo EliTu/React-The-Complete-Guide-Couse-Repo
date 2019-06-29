53820f45fe3a83ea0870e71458b2cf6684878cb7
EliTu <eliad91@gmail.com>
2019-06-29 15:09:39 +0800

    Finished lecture 285

--- Section 14/redux--01-start/src/index.js
+++ Section 14/redux--01-start/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import reducer from './store/reducer';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer);
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

b563d1e06ef4a418cae3a79e63bac21d66305a0e
EliTu <eliad91@gmail.com>
2019-06-29 15:34:58 +0800

    Finished lecture 286

--- Section 14/redux--01-start/src/store/reducer.js
+++ Section 14/redux--01-start/src/store/reducer.js
@@ -1,7 +1,9 @@
+// The initial state set in the Redux store:
 const initialState = {
 	counter: 0,
 };
 
+// The main reducer function:
 const reducer = (state = initialState, action) => {
 	return state;
 };

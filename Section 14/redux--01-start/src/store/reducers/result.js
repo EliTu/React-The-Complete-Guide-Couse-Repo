9090db9b2c2721156048c189ecf80d78bd93de3e
EliTu <eliad91@gmail.com>
2019-07-01 18:03:27 +0800

    Finished lecture 294

---
+++ Section 14/redux--01-start/src/store/reducers/result.js
@@ -0,0 +1,34 @@
+import * as actions from '../actions';
+
+const initialState = {
+	results: [],
+};
+
+const resultReducer = (state = initialState, action) => {
+	const { STORE_RESULT, DEL_RESULT } = actions;
+
+	switch (action.type) {
+		case STORE_RESULT:
+			return {
+				...state,
+				results: state.results.concat({
+					value: action.result,
+					id: new Date(),
+				}),
+			};
+
+		case DEL_RESULT:
+			const updatedArr = state.results.filter(
+				result => result.id !== action.id
+			);
+			return {
+				...state,
+				results: updatedArr,
+			};
+
+		default:
+			return state;
+	}
+};
+
+export default resultReducer;

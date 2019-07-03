0277a171c89b5f50ef1af8e224cac0527e8d3a40
EliTu <eliad91@gmail.com>
2019-07-03 14:58:57 +0800

    Finished lecture 300, preparing to pass some app local state to a redux store

---
+++ Section 15/burger-builder-app-section15/src/store/actions.js
@@ -0,0 +1,4 @@
+export const BurgerBuilderActions = {
+	ADD_INGREDIENT: 'ADD_INGREDIENT',
+	REMOVE_INGREDIENT: 'REMOVE_INGREDIENT',
+};
---
+++ Section 15/burger-builder-app-section15/src/store/reducer.js
@@ -0,0 +1,12 @@
+import { BurgerBuilderActions as actionTypes } from './actions';
+
+const INITIAL_STATE = {
+	ingredients: null,
+	totalPrice: 3,
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+	return state;
+};
+
+export default reducer;

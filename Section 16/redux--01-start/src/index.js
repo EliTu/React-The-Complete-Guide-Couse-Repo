34a07f248eeacaaff43dbbd1bd068ade3d51f1a6
EliTu <eliad91@gmail.com>
2019-07-04 17:37:52 +0800

    Finished lecture 312

--- Section 16/redux--01-start/src/index.js
+++ Section 16/redux--01-start/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -9,13 +9,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const rootReducer = combineReducers({
 	counterReducer: counterReducer,
 	resultReducer: resultReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(logger));
-// console.log(store.getState());
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(logger))
+);
 
 ReactDOM.render(
 	<Provider store={store}>

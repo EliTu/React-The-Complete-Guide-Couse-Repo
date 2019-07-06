2744c4eda30c9cf5425dc4b6e6498f0072f864a2
EliTu <eliad91@gmail.com>
2019-07-06 11:11:28 +0800

    Finished lecture 316

--- Section 16/redux--01-start/src/index.js
+++ Section 16/redux--01-start/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 import logger from './store/middleware';
@@ -18,7 +19,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
 	rootReducer,
-	composeEnhancers(applyMiddleware(logger))
+	composeEnhancers(applyMiddleware(logger, thunk))
 );
 
 ReactDOM.render(

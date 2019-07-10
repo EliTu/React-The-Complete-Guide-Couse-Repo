d4af045611e8acd59717196d2d3a55ff30089f85
EliTu <eliad91@gmail.com>
2019-07-10 16:01:19 +0800

    Finished lecture 334, adding a store, actions and constants for the OrderForm container

---
+++ Section 17/burger-builder-app-section17/src/components/CheckoutSummary/ContactData/store/actions.js
@@ -0,0 +1,29 @@
+import { PURCHASE_BURGER_SUCCESS, PURCHASE_BURGER_FAIL } from './constants';
+import axiosInstance from '../../../../axios-orders';
+
+export const purchaseBurgerSuccess = (id, order) => {
+	return {
+		type: PURCHASE_BURGER_SUCCESS,
+		orderId: id,
+		orderData: order,
+	};
+};
+
+export const purchaseBurgerFail = error => {
+	return {
+		type: PURCHASE_BURGER_FAIL,
+		error: error,
+	};
+};
+
+export const postPurchasedBurger = order => {
+	return async dispatch => {
+		try {
+			const postRequest = await axiosInstance.post('/orders.json', order);
+			console.log(postRequest);
+			dispatchEvent(purchaseBurgerSuccess(postRequest.id, order));
+		} catch (error) {
+			dispatchEvent(purchaseBurgerFail(error));
+		}
+	};
+};

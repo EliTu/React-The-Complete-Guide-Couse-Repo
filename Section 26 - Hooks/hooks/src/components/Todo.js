a8cc3784d28bc43b4a097eb8f6e15b50b4833ff0
EliTu <eliad91@gmail.com>
2019-08-02 18:19:51 +0800

    Finished lecture 459

---
+++ Section 26 - Hooks/hooks/src/components/Todo.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Todo = () => {
+	const [todos, setTodos] = useState([]);
+	const [input, setInput] = useState('');
+
+	const handleInputChange = e => setInput(e.target.value);
+
+	const handleButtonClick = () => setTodos([...todos, input]);
+
+	return (
+		<>
+			<input
+				type="text"
+				placeholder="Todo"
+				onChange={e => handleInputChange(e)}
+			/>
+			<button type="button" onClick={handleButtonClick}>
+				Add
+			</button>
+			<ul>
+				{todos.map((el, i) => (
+					<li key={i}>{el}</li>
+				))}
+			</ul>
+		</>
+	);
+};
+export default Todo;

109fb7ab35f50d25a935e3c412dbeb99f1bae28b
EliTu <eliad91@gmail.com>
2019-08-04 13:09:50 +0800

    Finished lecture 465

--- Section 26 - Hooks/hooks/src/components/Todo.js
+++ Section 26 - Hooks/hooks/src/components/Todo.js
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Todo = () => {
 	const [todos, setTodos] = useState([]);
 	const [input, setInput] = useState('');
 
-	const handleNewTodoItem = () => {
-		setTodos([...todos, input]);
-		setTimeout(() => {
-			try {
-				console.log(`ADDED ASYNC ${input} `);
-			} catch (error) {
-				console.log(error);
-			}
-		}, 2000);
+	const handleNewTodoItem = async () => {
+		try {
+			await axios.post(
+				'https://react-hooks-intro-940a4.firebaseio.com/todos.json',
+				{ todo: input }
+			);
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	useEffect(() => {
-		setTimeout(() => {
-			console.log(`FETCHED ${input} ON MOUNT`);
-		}, 2500);
+		const getTodoData = async () => {
+			try {
+				const todoData = await axios.get(
+					'https://react-hooks-intro-940a4.firebaseio.com/todos.json'
+				);
+				const todoItems = todoData.data;
+				let newTodoList = [];
+				for (let item in todoItems) {
+					newTodoList.push({ id: item, name: todoItems[item].todo });
+				}
+				setTodos(newTodoList);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getTodoData();
 	}, [todos]);
 
 	const handleInputChange = e => setInput(e.target.value);
@@ -27,6 +41,12 @@ const Todo = () => {
 
 	const handleInputEnterPress = e =>
 		e.key === 'Enter' ? handleNewTodoItem() : null;
+
+	const handleClearButtonClick = async () => {
+		await axios.delete(
+			'https://react-hooks-intro-940a4.firebaseio.com/todos.json'
+		);
+	};
 
 	return (
 		<>
@@ -40,10 +60,10 @@ const Todo = () => {
 			<button type="button" onClick={handleAddButtonClick}>
 				Add
 			</button>
-			<button onClick={() => setTodos([])}>Clear</button>
+			<button onClick={handleClearButtonClick}>Clear</button>
 			<ul>
-				{todos.map((todo, i) => (
-					<li key={i}>{todo}</li>
+				{todos.map(todo => (
+					<li key={todo.id}>{todo.name}</li>
 				))}
 			</ul>
 		</>

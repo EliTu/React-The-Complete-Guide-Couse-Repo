ce51d3abc6af4e5399cfb48d3961c1be98f15430
EliTu <eliad91@gmail.com>
2019-08-04 11:38:49 +0800

    Finished lecture 462

--- Section 26 - Hooks/hooks/src/components/Todo.js
+++ Section 26 - Hooks/hooks/src/components/Todo.js
@@ -8,6 +8,9 @@ const Todo = () => {
 
 	const handleButtonClick = () => setTodos([...todos, input]);
 
+	const handleEnterPress = e =>
+		e.key === 'Enter' ? setTodos([...todos, input]) : null;
+
 	return (
 		<>
 			<input
@@ -15,14 +18,15 @@ const Todo = () => {
 				placeholder="Todo"
 				value={input}
 				onChange={e => handleInputChange(e)}
+				onKeyPress={e => handleEnterPress(e)}
 			/>
 			<button type="button" onClick={handleButtonClick}>
 				Add
 			</button>
 			<button onClick={() => setTodos([])}>Clear</button>
 			<ul>
-				{todos.map((el, i) => (
-					<li key={i}>{el}</li>
+				{todos.map((todo, i) => (
+					<li key={i}>{todo}</li>
 				))}
 			</ul>
 		</>

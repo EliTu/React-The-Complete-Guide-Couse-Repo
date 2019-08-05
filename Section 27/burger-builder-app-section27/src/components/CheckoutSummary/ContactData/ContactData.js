45ddb4430071fe430fa60cc45dcd502d166b173b
EliTu <eliad91@gmail.com>
2019-08-05 20:37:10 +0800

    Converted ContactData from a class based component to a functional stateful component

--- Section 27/burger-builder-app-section27/src/components/CheckoutSummary/ContactData/ContactData.js
+++ Section 27/burger-builder-app-section27/src/components/CheckoutSummary/ContactData/ContactData.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { postPurchasedBurger } from './store/actions';
 import Button from '../../UI/Button/Button';

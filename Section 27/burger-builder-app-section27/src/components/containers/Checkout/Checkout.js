8a0e6b6828098b46ffa1c5929c92643ddb7b54ee
EliTu <eliad91@gmail.com>
2019-08-10 16:14:55 +0800

    Converted Checkout component from class based to a stateful functional component

--- Section 27/burger-builder-app-section27/src/components/containers/Checkout/Checkout.js
+++ Section 27/burger-builder-app-section27/src/components/containers/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../CheckoutSummary/CheckoutSummary';
@@ -6,40 +6,35 @@ import ContactData from '../../CheckoutSummary/ContactData/ContactData';
 import styles from './Checkout.module.css';
 import PropTypes from 'prop-types';
 
-export class Checkout extends Component {
-	handleCancelClick = () => {
-		return this.props.history.goBack();
+export const Checkout = ({ ingredients, history, match }) => {
+	const handleCancelClick = () => {
+		return history.goBack();
 	};
 
-	handleCheckoutClick = () => {
-		return this.props.history.replace('/checkout/contact-data');
+	const handleCheckoutClick = () => {
+		return history.replace('/checkout/contact-data');
 	};
 
-	render() {
-		// CSS Modules styles:
-		const { Checkout, CheckoutHeader } = styles;
+	// CSS Modules styles:
+	const { Checkout, CheckoutHeader } = styles;
 
-		// state:
-		const { ingredients } = this.props;
-
-		return (
-			<>
-				<h1 className={CheckoutHeader}>Your burger:</h1>
-				<div className={Checkout}>
-					<CheckoutSummary
-						ingredients={ingredients}
-						cancelClick={this.handleCancelClick}
-						checkoutClick={this.handleCheckoutClick}
-					/>
-					<Route
-						path={`${this.props.match.path}/contact-data`}
-						component={ContactData}
-					/>
-				</div>
-			</>
-		);
-	}
-}
+	return (
+		<>
+			<h1 className={CheckoutHeader}>Your burger:</h1>
+			<div className={Checkout}>
+				<CheckoutSummary
+					ingredients={ingredients}
+					cancelClick={handleCancelClick}
+					checkoutClick={handleCheckoutClick}
+				/>
+				<Route
+					path={`${match.path}/contact-data`}
+					component={ContactData}
+				/>
+			</div>
+		</>
+	);
+};
 
 Checkout.propTypes = {
 	ingredients: PropTypes.array,

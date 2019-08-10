7040a2e67d69bf26b296b90739bc281af041f714
EliTu <eliad91@gmail.com>
2019-08-10 16:38:22 +0800

    Converted requestErrorMessage HOC into a functional stateful component

--- Section 27/burger-builder-app-section27/src/components/hoc/requestMessageComponent/requestMessageComponent.js
+++ Section 27/burger-builder-app-section27/src/components/hoc/requestMessageComponent/requestMessageComponent.js
@@ -1,84 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '../../UI/Modal/Modal';
 import styles from './requestMessageComponent.module.css';
 
 const requestMessageComponent = (WrappedComponent, axiosInstance) => {
-	return class extends Component {
-		state = {
-			error: null,
-			responseStatus: false,
-		};
+	return ({ ...props }) => {
+		// Local state hooks:
+		const [error, setError] = useState(null);
+		const [responseStatus, setResponseStatus] = useState(false);
 
-		componentDidMount() {
-			this.requestInterceptor = axiosInstance.interceptors.request.use(
+		useEffect(() => {
+			const requestInterceptor = axiosInstance.interceptors.request.use(
 				request => {
-					this.setState({
-						error: null,
-					});
+					setError(null);
 					return request;
 				}
 			);
 
-			this.responseInterceptor = axiosInstance.interceptors.response.use(
-				res => {
-					this.setState({
-						responseStatus: res.status === 200 ? true : false,
-					});
-				},
-				error => {
-					this.setState({
-						error: error,
-					});
-				}
+			const responseInterceptor = axiosInstance.interceptors.response.use(
+				res => setResponseStatus(res.status === 200 ? true : false),
+				error => setError(error)
 			);
-		}
 
-		componentWillUnmount() {
-			// Remove the interceptors upon unmounting the component
-			axiosInstance.interceptors.response.eject(this.responseInterceptor);
-			axiosInstance.interceptors.request.eject(this.requestInterceptor);
-		}
+			return () => {
+				// Remove the interceptors upon unmounting the component
+				axiosInstance.interceptors.response.eject(responseInterceptor);
+				axiosInstance.interceptors.request.eject(requestInterceptor);
+			};
+		}, [error, responseStatus]);
 
-		handleErrorConfirmClick = () => {
+		const handleErrorConfirmClick = () => {
 			this.setState({
 				error: null,
 				responseStatus: false,
 			});
 		};
 
-		handleButton;
+		// CSS Modules styles:
+		const { ErrorText, confirmationText } = styles;
 
-		render() {
-			// State:
-			const { error, responseStatus } = this.state;
+		const errorMessage = error ? (
+			<p className={ErrorText}>
+				Oops! Something went wrong :\
+				<span>({error.message})</span>
+			</p>
+		) : null;
 
-			// CSS Modules styles:
-			const { ErrorText, confirmationText } = styles;
+		const confirmationMessage = responseStatus ? (
+			<p className={confirmationText}>Received</p>
+		) : null;
 
-			const errorMessage = error ? (
-				<p className={ErrorText}>
-					Oops! Something went wrong :\
-					<span>({error.message})</span>
-				</p>
-			) : null;
-
-			const confirmationMessage = responseStatus ? (
-				<p className={confirmationText}>Received</p>
-			) : null;
-
-			return (
-				<>
-					<Modal
-						show={error || responseStatus}
-						closeModalHandler={this.handleErrorConfirmClick}
-					>
-						{confirmationMessage}
-						{errorMessage}
-					</Modal>
-					<WrappedComponent {...this.props} />
-				</>
-			);
-		}
+		return (
+			<>
+				<Modal
+					show={error || responseStatus}
+					closeModalHandler={handleErrorConfirmClick}
+				>
+					{confirmationMessage}
+					{errorMessage}
+				</Modal>
+				<WrappedComponent {...props} />
+			</>
+		);
 	};
 };
 export default requestMessageComponent;

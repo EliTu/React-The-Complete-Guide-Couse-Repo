a522dad5d58a9c84b4fd5975d74edc1bc78e42ef
EliTu <eliad91@gmail.com>
2019-08-14 16:15:11 +0800

    Setup the NotificationBox basic functionality

--- Section 27/burger-builder-app-section27/src/components/containers/NotificationBox/NotificationBox.js
+++ Section 27/burger-builder-app-section27/src/components/containers/NotificationBox/NotificationBox.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import styles from './NotificationBox.module.css';
+import PropTypes from 'prop-types';
 
-const NotificationBox = () => {
-	const { NotificationBox } = styles;
+const NotificationBox = ({
+	isLoggedIn,
+	authType,
+	isErrorOnMount,
+	isLoadingRequest,
+}) => {
+	// CSS Modules styles:
+	const { NotificationBox, Open, Closed } = styles;
+
+	// Local state hooks:
+	const [isDisplayed, setIsDisplayed] = useState(false);
+
+	const displayStatus = isDisplayed ? Open : Closed;
+
+	useEffect(() => {
+		setIsDisplayed(true);
+	}, [isLoggedIn, isErrorOnMount, isLoadingRequest]);
+
 	return (
-		<div className={NotificationBox}>
+		<div
+			className={[NotificationBox, displayStatus].join(' ')}
+			onClick={() => setIsDisplayed(false)}
+		>
 			<p>Test</p>
 		</div>
 	);
 };
 
-export default connect()(NotificationBox);
+NotificationBox.propTypes = {
+	authType: PropTypes.string,
+	isLoggedIn: PropTypes.bool,
+	isErrorOnMount: PropTypes.bool,
+	isLoadingRequest: PropTypes.bool,
+};
+
+// Redux setup:
+const mapStateToProps = state => {
+	return {
+		isLoggedIn: state.auth.isLoggedIn,
+		authType: state.auth.authType,
+		isErrorOnMount: state.burgerBuilder.isErrorOnMount,
+		isLoadingRequest: state.orderForm.isLoading,
+	};
+};
+
+export default connect(mapStateToProps)(NotificationBox);

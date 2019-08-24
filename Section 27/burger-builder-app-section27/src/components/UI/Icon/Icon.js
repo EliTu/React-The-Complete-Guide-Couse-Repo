e074d1bfda220d45f100394e82b0a01b9893f39b
EliTu <eliad91@gmail.com>
2019-08-24 17:35:05 +0800

    Testing Icon component

--- Section 27/burger-builder-app-section27/src/components/UI/Icon/Icon.js
+++ Section 27/burger-builder-app-section27/src/components/UI/Icon/Icon.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Icon.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
 
 const Icon = ({ iconType, size = '1x' }) => {
 	// CSS Module styles:
@@ -10,6 +11,11 @@ const Icon = ({ iconType, size = '1x' }) => {
 			<FontAwesomeIcon icon={iconType} size={size} />
 		</span>
 	);
+};
+
+Icon.propTypes = {
+	iconType: PropTypes.object,
+	size: PropTypes.string,
 };
 
 export default Icon;

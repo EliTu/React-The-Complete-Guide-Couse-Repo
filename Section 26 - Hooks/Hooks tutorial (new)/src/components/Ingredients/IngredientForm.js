0eecbd9a22b51a728158d3c926fcdb06eaf06159
EliTu <eliad91@gmail.com>
2019-08-29 12:46:55 +0800

    Lecutre 460

--- Section 26 - Hooks/Hooks tutorial (new)/src/components/Ingredients/IngredientForm.js
+++ Section 26 - Hooks/Hooks tutorial (new)/src/components/Ingredients/IngredientForm.js
@@ -1,33 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
-    event.preventDefault();
-    // ...
-  };
+	const [title, setTitle] = useState('');
+	const [number, setNumber] = useState('');
+	const [ingredients, setIngredients] = useState([]);
 
-  return (
-    <section className="ingredient-form">
-      <Card>
-        <form onSubmit={submitHandler}>
-          <div className="form-control">
-            <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
-          </div>
-          <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
-          </div>
-        </form>
-      </Card>
-    </section>
-  );
+	const handleChange = (event, id) =>
+		id === 'title'
+			? setTitle(event.target.value)
+			: setNumber(event.target.value);
+
+	const submitHandler = event => {
+		event.preventDefault();
+		setIngredients([...ingredients, { title: title, number: number }]);
+	};
+
+	return (
+		<section className="ingredient-form">
+			<Card>
+				<form onSubmit={submitHandler}>
+					<div className="form-control">
+						<label htmlFor="title">Name</label>
+						<input
+							type="text"
+							id="title"
+							value={title}
+							onChange={event => handleChange(event, 'title')}
+						/>
+					</div>
+					<div className="form-control">
+						<label htmlFor="amount">Amount</label>
+						<input
+							type="number"
+							id="amount"
+							value={number}
+							onChange={event => handleChange(event, 'number')}
+						/>
+					</div>
+					<div className="ingredient-form__actions">
+						<button type="submit">Add Ingredient</button>
+					</div>
+				</form>
+			</Card>
+		</section>
+	);
 });
 
 export default IngredientForm;

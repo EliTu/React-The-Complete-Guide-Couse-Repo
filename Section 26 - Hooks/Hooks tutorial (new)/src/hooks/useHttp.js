9764e217fea20384dec831525b4f4c3f134a5874
EliTu <eliad91@gmail.com>
2019-09-11 12:56:19 +0300

    Finished lecture 480

---
+++ Section 26 - Hooks/Hooks tutorial (new)/src/hooks/useHttp.js
@@ -0,0 +1,51 @@
+import { useState, useCallback } from 'react';
+
+const useHttp = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
+
+	const handleHttpRequest = useCallback(
+		async (url, httpVerb, httpBody, ing, dispatchFn) => {
+			setIsLoading(true);
+			try {
+				const { name: firebaseId } = await fetch(url, {
+					method: `${httpVerb}`,
+					body: httpBody,
+					headers: { 'Content-Type': 'application/json' },
+				}).then(response => {
+					setIsLoading(false);
+					console.log(response);
+					return response.json();
+				});
+
+				if (firebaseId) {
+					dispatchFn({
+						type: 'ADD',
+						newIngredient: { id: firebaseId, ...ing },
+					});
+				}
+			} catch (error) {
+				setIsLoading(false);
+				error && setIsError(true);
+				setErrorMessage('Server error!');
+			}
+		},
+		[]
+	);
+
+	const handleErrorClear = () => {
+		setIsError(false);
+		setErrorMessage('');
+	};
+
+	return [
+		isLoading,
+		isError,
+		errorMessage,
+		handleHttpRequest,
+		handleErrorClear,
+	];
+};
+
+export default useHttp;
